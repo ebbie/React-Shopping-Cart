9aac5e1cb6f87b7a2e44f6a7e3d6961fe7f9f85b
Ebbie <ebenezer.arapally@gmail.com>
2020-08-31 00:18:05 -0400

    add animation and modal

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade'
 
 export default class Cart extends Component {
     constructor(props){
@@ -36,6 +37,7 @@ export default class Cart extends Component {
                 )}
                 <div>
                 <div className="cart">
+                    <Fade left cascade>
                     <ul className="cart-items">
                         {cartItems.map(item => 
                             <li key={item._id}>
@@ -55,6 +57,7 @@ export default class Cart extends Component {
                             </li>
                             )}
                     </ul>
+                    </Fade>
 
                 </div>
                 {cartItems.length!==0 && (
@@ -73,6 +76,7 @@ export default class Cart extends Component {
                     </div>
                 </div>
                 {this.state.showCheckout && (
+                    <Fade right cascade>
                     <div className="cart">
                         <form onClick={this.createOrder}>
                             <ul className="form-container">
@@ -110,6 +114,7 @@ export default class Cart extends Component {
 
                         </form>
                     </div>
+                </Fade>
 
                 )}
                 </div>
