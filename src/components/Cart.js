309cc455a966e6f20758be772dca145b282ae6c7
Ebbie <ebenezer.arapally@gmail.com>
2020-09-01 23:21:52 -0400

    add create order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
 import Fade from 'react-reveal/Fade'
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom"
 import { connect } from 'react-redux';
 import { removeFromCart } from '../actions/cartActions';
+import { createOrder, clearOrder } from "../actions/orderActions"
 
 
 class Cart extends Component {
@@ -24,20 +27,73 @@ class Cart extends Component {
             email: this.state.email,
             address: this.state.address,
             cartItems: this.props.cartItems,
+            total: this.props.cartItems.reduce((a,c)=> a + c.price * c.count, 0),
         };
         this.props.createOrder(order);
     };
 
+    closeModal = () =>{
+        this.props.clearOrder();
+    };
+
     render() {
-        const {cartItems} = this.props;
+        const {cartItems, order} = this.props;
         return (
             <div>
-                {cartItems.length === 0? <div className="cart cart-header"> Cart is empty</div>
+                {cartItems.length === 0? ( <div className="cart cart-header"> Cart is empty</div> )
                 : (
                 <div className="cart cart-header">
                     You have {cartItems.length} unique products in the cart{" "}
                 </div>
                 )}
+
+                {
+                    order && (<Modal
+                    isOpen={true}>
+                        <Zoom>
+                            <button className="close-modal" onClick={this.closeModal}>x</button>
+                            <div className="order-details">
+                                <h3 className="success-message">Your order has been placed.</h3>
+                                <h2>
+                                    Order {order._id}
+                                </h2>
+                                <ul>
+                                    <li>
+                                        <div>Name:</div>
+                                        <div>{order.name}</div>
+                                    </li>
+                                    <li>
+                                        <div>Email:</div>
+                                        <div>{order.email}</div>
+                                    </li>
+                                    <li>
+                                        <div>Address:</div>
+                                        <div>{order.address}</div>
+                                    </li>
+                                    <li>
+                                        <div>Date:</div>
+                                        <div>{order.createdAt}</div>
+                                    </li>
+                                    <li>
+                                        <div>Total:</div>
+                                        <div>{formatCurrency(order.total)}</div>
+                                    </li>
+                                    <li>
+                                        <div>Cart Items:</div>
+                                        <div>
+                                            {order.cartItems.map((x) => (
+                                                <div>
+                                                    {x.count} {" x "} {x.title}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </Zoom>
+                    </Modal>
+                    )}
                 <div>
                 <div className="cart">
                     <Fade left cascade>
@@ -81,7 +137,7 @@ class Cart extends Component {
                 {this.state.showCheckout && (
                     <Fade right cascade>
                     <div className="cart">
-                        <form onClick={this.createOrder}>
+                        <form>
                             <ul className="form-container">
                                 <li>
                                     <label>Email</label>
@@ -95,7 +151,7 @@ class Cart extends Component {
                                 <li>
                                     <label>Name</label>
                                     <input 
-                                    address="name"
+                                    name="name"
                                     type ="text"
                                     required
                                     onChange={this.handleInput}
@@ -111,7 +167,7 @@ class Cart extends Component {
                                     ></input>
                                 </li>
                                 <li>
-                                    <button className="button primary" type="submit">Checkout</button>
+                                    <button className="button primary" type="submit" onClick={this.createOrder}>Checkout</button>
                                 </li>
                             </ul>
 
@@ -131,7 +187,8 @@ class Cart extends Component {
 }
 
 export default connect((state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems,
 }),
-    {removeFromCart}
+    {removeFromCart, createOrder, clearOrder}
 )(Cart);
